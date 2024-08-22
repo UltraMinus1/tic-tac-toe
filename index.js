b367ed483e57af3027610bd02a05ddb3605ef84c
Michal Szczegielniak <ultraminus11@gmail.com>
2024-08-22 12:11:21 +0200

    created basic game board ui

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function makeGameBoard() {
-    const board = [[null, null, null],
-                   [null, null, null],
-                   [null, null, null]];
+    const board = [null, null, null,
+                   null, null, null,
+                   null, null, null];
     
-    const place = (marker, X, Y) => {
-        if (board[Y][X] == null) {
-            board[Y][X] = marker;
+    const place = (marker, pos) => {
+        if (board[pos] == null) {
+            board[pos] = marker;
             return true;
         } else {
             return false;
@@ -17,61 +17,64 @@ function makeGameBoard() {
 
 
 function makePlayer(PlayerName, marker) {
-    const placeYourMarker = (X, Y, board) => {
-        return board.place(marker, X, Y);
+    const placeYourMarker = (pos, board) => {
+        return board.place(marker, pos);
     }
     return {PlayerName, marker, placeYourMarker};
 }
 
-function makeGame(player1Name, player2Name) {
+function makeGame(Player1, Player2) {
+    let finshed = false;
     let board = makeGameBoard();
-    const Player1 = makePlayer(player1Name, "X");
-    const Player2 = makePlayer(player2Name, "O");
     let currentTurn = 0;
     let turnCount = 0;
-    const playTurn = (X, Y) => {
-        if (currentTurn == 0) {
-            let correct = Player1.placeYourMarker(X, Y, board);
-            if (correct) {
-                currentTurn = 1;
-                turnCount++;
-                var victor = checkForWin();
+    const playTurn = (pos) => {
+        if (!finshed) {
+            if (currentTurn == 0) {
+                let correct = Player1.placeYourMarker(pos, board);
+                if (correct) {
+                    currentTurn = 1;
+                    turnCount++;
+                    var victor = checkForWin();
+                }
+            } else if (currentTurn == 1) {
+                let correct = Player2.placeYourMarker(pos, board)
+                if (correct) {
+                    currentTurn = 0;
+                    turnCount++;
+                    var victor = checkForWin();
+                }
             }
-        } else if (currentTurn == 1) {
-            let correct = Player2.placeYourMarker(X, Y, board)
-            if (correct) {
-                currentTurn = 0;
-                turnCount++;
-                var victor = checkForWin();
+            getBoard();
+            if (victor != null) {
+                endGame(victor);
             }
         }
-        getBoard();
-        if (victor != null) {
-            endGame(victor);
-        }
+
     }
     const getBoard = () => {
-        console.log(board.board)
+        //console.log(board.board);
+        return board.board
     }
 
-    const winning = [[[0, 0], [1, 0], [2, 0]],
-                     [[0, 1], [1, 1], [2, 1]],
-                     [[0, 2], [1, 2], [2, 2]],
-                     [[0, 0], [0, 1], [0, 2]],
-                     [[1, 0], [1, 1], [1, 2]],
-                     [[2, 0], [2, 1], [2, 2]],
-                     [[0, 0], [1, 1], [2, 2]],
-                     [[2, 0], [1, 1], [0, 2]]];
+    const winning = [[0, 1, 2],
+                     [3, 4, 5],
+                     [6, 7, 8],
+                     [0, 3, 6],
+                     [1, 4, 7],
+                     [2, 5, 8],
+                     [0, 4, 8],
+                     [2, 4, 6]];
 
     const checkForWin = () => {
         for (let position of winning ) {
             let Ocount = 0;
             let Xcount = 0;
             for (let i = 0; i < 3; i++) {
-                let [X, Y] = position[i];
-                if (board.board[Y][X] == "X") {
+                let pos = position[i];
+                if (board.board[pos] == "X") {
                     Xcount++;
-                } else if (board.board[Y][X] == "O") {
+                } else if (board.board[pos] == "O") {
                     Ocount++;
                 }
             }
@@ -97,21 +100,51 @@ function makeGame(player1Name, player2Name) {
             console.log("It's a draw")
         }
         // this will be changed
-        board = makeGameBoard();
-        currentTurn = 0;
-        turnCount = 0;
+        //board = makeGameBoard();
+        //currentTurn = 0;
+        //turnCount = 0;
+        finshed = true
     }
     
     const getTurnCount = () => {
         return turnCount
     }
 
-    return {getBoard, playTurn, checkForWin, getTurnCount, turnCount}
+    return {getBoard, playTurn}
 }
 
 
+const UIrenderer = function () {
+    const runGame = (Player1, Player2) => {
+        let game = makeGame(Player1, Player2);
+        
+    }
+
+    const renderBoard = (game) => {
+        let boardArr = game.getBoard();
+        const boardEl = document.querySelector(".board")
+        boardEl.replaceChildren();
+        for (let i = 0; i < 9; i++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.dataset.position = i;
+            square.textContent = boardArr[i];
+            square.addEventListener("click", (ref) => {
+                pos = ref.target.getAttribute("data-position");
+                //console.log(pos)
+                game.playTurn(pos);
+                renderBoard(game);
+            })
+            boardEl.appendChild(square);
+        }
+        
+    }
+    return {renderBoard}
+}();
+
+let Player1 = makePlayer("test", "X");
+let Player2 = makePlayer("test2", "O");
+let game = makeGame(Player1, Player2);
 
 
-
-
-
+UIrenderer.renderBoard(game);
